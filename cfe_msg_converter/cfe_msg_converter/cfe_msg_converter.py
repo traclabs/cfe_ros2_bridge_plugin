432461e58ffa9847b2413d81538ad07c4138475b
Stephen Hart <swhart@traclabs.com>
2022-08-12 11:23:01 -0500

    added pytest stuff for juicer and converter packages. did pystyleguide compliance

--- cfe_msg_converter/cfe_msg_converter/cfe_msg_converter.py
+++ cfe_msg_converter/cfe_msg_converter/cfe_msg_converter.py
@@ -10,19 +10,19 @@ class CfeMsgConverter(Node):
         super().__init__('cfe_msg_converter')
 
         self.declare_parameter('cfs_root', 'cfe_msg_converter.cfs_root')
-        self.cfs_root = self.get_parameter('cfs_root').get_parameter_value().string_value
-        if '~' in self.cfs_root:
-            self.cfs_root = os.path.expanduser(self.cfs_root)
-        self.get_logger().info("cfs_root: " + self.cfs_root)
+        self._cfs_root = self.get_parameter('cfs_root').get_parameter_value().string_value
+        if '~' in self._cfs_root:
+            self._cfs_root = os.path.expanduser(self._cfs_root)
+        self.get_logger().info("cfs_root: " + self._cfs_root)
 
     def create_messages(self, msgs_dir):
-        for key in self.symbol_name_map.keys():
-            if self.symbol_name_map[key].get_should_output():
-                self.output_msg_file(self.symbol_name_map[key], msgs_dir)
+        for key in self._symbol_name_map.keys():
+            if self._symbol_name_map[key].get_should_output():
+                self.output_msg_file(self._symbol_name_map[key], msgs_dir)
 
         msg_list = []
-        for key in self.symbol_name_map.keys():
-            symbol = self.symbol_name_map[key]
+        for key in self._symbol_name_map.keys():
+            symbol = self._symbol_name_map[key]
             if len(symbol.get_fields()) > 0 and symbol.get_should_output():
                 mn = symbol.get_ros_name()
                 if mn[0].isupper():
