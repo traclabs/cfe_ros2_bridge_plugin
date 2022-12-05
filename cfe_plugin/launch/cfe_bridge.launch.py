2a09a8ff2fea54875def5d9971e4bb26eea42fc5
Tod Milam <tmilam@traclabs.com>
2022-12-05 14:03:32 -0600

    Cleanup launch file.

--- cfe_plugin/launch/cfe_bridge.launch.py
+++ cfe_plugin/launch/cfe_bridge.launch.py
@@ -2,19 +2,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 
 
 def generate_launch_description():
     ld = LaunchDescription()
-    juicer_db = LaunchConfiguration("juicer_db")
-    commands = LaunchConfiguration("commands")
-    telemetry = LaunchConfiguration("telemetry")
-    command_data = LaunchConfiguration("command_data")
-    telemetry_data = LaunchConfiguration("telemetry_data")
-    juicer_package = LaunchConfiguration("juicer_package")
-
     config = os.path.join(
         get_package_share_directory('cfe_plugin'),
         'config',
