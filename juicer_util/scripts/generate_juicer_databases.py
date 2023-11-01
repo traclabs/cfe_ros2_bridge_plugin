2f9c5ea4e1d16db353a1c52259d219c0bb4c7629
Stephen Hart <swhart@traclabs.com>
2023-11-01 15:21:55 -0400

    added some generality to the generate juicer db script

--- juicer_util/scripts/generate_juicer_databases.py
+++ juicer_util/scripts/generate_juicer_databases.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
+import getopt
 
 # This script runs juicer over a list of input files.
 
-class RunMultipleFiles():
-
-    def __init__(self):
-        self.files = ['core-cpu1', 'cf/cfe_assert.so', 'cf/ci_lab.so',
+files_list = ['core-cpu1', 'cf/cfe_assert.so', 'cf/ci_lab.so',
                       'cf/ros_app.so', 'cf/sample_app.so', 'cf/sample_lib.so',
                       'cf/sbn_f_remap.so', 'cf/sbn.so', 'cf/sbn_udp.so',
                       'cf/sch_lab.so', 'cf/to_lab.so', 'cf/robot_sim.so',
                       'cf/cf.so']
-        self.fileDir = '/home/swhart/code/cFS/build/exe/cpu1'
-        self.outDir = '/home/swhart/code/juicer/dbs'
-        self.exeCmd = '/home/swhart/code/juicer/build/juicer'
+
+class RunMultipleFiles():
+
+    def __init__(self, cfe_path, juicer_path):
+        self.files = files_list
+        self.fileDir = cfe_path + '/build/exe/cpu1'
+        self.outDir = juicer_path + '/dbs'
+        self.exeCmd = juicer_path + '/build/juicer'
 
         for name in self.files:
             fname = self.fileDir + "/" + name
@@ -26,9 +30,39 @@ class RunMultipleFiles():
             subprocess.run(runStr, shell=True)
 
 
-def main():
-    RunMultipleFiles()
+def main(argv):
+
+    arg_cfe_path = ""
+    arg_juicer_path = ""
+    arg_help = "{0} -c <cfe_path> -j <juicer_path>".format(argv[0])
+
+    try:
+        opts, args = getopt.getopt(argv[1:], "hc:j:", ["help", "cfe_path=", "juicer_path="])
+    except:
+        print(arg_help)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(arg_help)  # print the help message
+            sys.exit(2)
+        elif opt in ("-c", "--cfe_path"):
+            arg_cfe_path = arg
+        elif opt in ("-j", "--juicer_path"):
+            arg_juicer_path = arg
+
+    if not arg_cfe_path:
+        print(arg_help)
+        print("please define the path of your cFS install")
+        sys.exit(2)
+
+    if not arg_juicer_path:
+        print(arg_help)
+        print("please define the path of your juicer install")
+        sys.exit(2)
+
+    RunMultipleFiles(arg_cfe_path, arg_juicer_path)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
